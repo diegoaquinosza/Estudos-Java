ac504a75d4d56fe55851cc15097463d138e079ff
diegoaquinosza <diegoaquinosza@gmail.com>
2025-12-17 18:42:08 -0300

    feat(Tarefas): finaliza gerenciador com lógica de remover e concluir itens

--- Curso extra senac/src/exercicio_tarefas/GerenciadorTarefas.java
+++ Curso extra senac/src/exercicio_tarefas/GerenciadorTarefas.java
@@ -23,18 +23,71 @@ public class GerenciadorTarefas {
             switch (opcao) {
                 case "1":
                     System.out.println("\n--- Adicionar tarefa ---");
+                    System.out.print("Digite o nome da tarefa: ");
+                    String newTarefa = input.nextLine();
+                    tarefas.add(newTarefa);
+                    System.out.print("Tarefa adicionada com sucesso!");
                     break;
-                    
+
                 case "2":
                     System.out.println("\n--- Remover tarefa ---");
+                    if (tarefas.isEmpty()) {
+                        System.out.println("Lista de tarefas está vázia!");
+                    } else {
+                        for (int i = 0; i < tarefas.size(); i++) {
+                            System.out.println("Nº " + (i + 1) + " - " + tarefas.get(i));
+                        }
+
+                        System.out.print("Digite o número da tarefa que quer remover: ");
+                        int delTarefa = input.nextInt();
+                        input.nextLine();
+
+                        if (delTarefa > 0 && delTarefa <= tarefas.size()) {
+                            tarefas.remove(delTarefa - 1);
+                            System.out.println("Tarefa removida com sucesso!");
+                        } else {
+                            System.out.println("número de tarefa inválido!");
+                        }
+
+                    }
                     break;
 
                 case "3":
                     System.out.println("\n--- Marcar tarefa como concluída ---");
+                    if (tarefas.isEmpty()) {
+                        System.out.println("Lista de tarefas está vázia!");
+                    } else {
+                        for (int i = 0; i < tarefas.size(); i++) {
+                            System.out.println("Nº " + (i + 1) + " - " + tarefas.get(i));
+                        }
+
+                        System.out.print("Digite o número da tarefa que quer concluir: ");
+                        int numOkTarefa = input.nextInt();
+                        input.nextLine();
+
+                        if (numOkTarefa > 0 && numOkTarefa <= tarefas.size()) {
+                            int indiceReal = numOkTarefa - 1;
+                            String tarefaAntiga = tarefas.get(indiceReal);
+                            String tarefaOk = "[OK]" + tarefaAntiga;
+                            tarefas.set(indiceReal, tarefaOk);
+                            System.out.println("Tarefa concluída com sucesso!");
+                        } else {
+                            System.out.println("número de tarefa inválido!");
+                        }
+
+                    }
+
                     break;
 
                 case "4":
                     System.out.println("\n--- Listar todas as tarefas ---");
+                    if (tarefas.isEmpty()) {
+                        System.out.println("Lista de tarefas está vázia!");
+                    } else {
+                        for (int i = 0; i < tarefas.size(); i++) {
+                            System.out.println("Nº " + (i + 1) + " - " + tarefas.get(i));
+                        }
+                    }
                     break;
 
                 case "5":
