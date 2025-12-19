99d262e0f09f446b48cdc80a18db6e3fbc645922
diegoaquinosza <diegoaquinosza@gmail.com>
2025-12-19 12:14:20 -0300

    refactor(Catraca): migra logica procedural para Orientacao a Objetos (catracapoo)

--- SenacCursoJava/Exercicio02CatracaOnibus/src/senac/curso/java/exercicio/catracapoo/Catraca.java
+++ SenacCursoJava/Exercicio02CatracaOnibus/src/senac/curso/java/exercicio/catracapoo/Catraca.java
@@ -1,4 +1,50 @@
 package senac.curso.java.exercicio.catracapoo;
 
 public class Catraca {
+    private int totalPassageiros;
+    private final int LIMITE = 45;
+    private boolean lotado;
+
+    public Catraca() {
+        this.totalPassageiros = 0;
+        this.lotado = false;
+    }
+
+    public void registrarEntrada() {
+        if (totalPassageiros == 45) {
+            lotado = true;
+            System.out.println("Ônibus lotado! Ninguém pode entrar.");
+        } else {
+            totalPassageiros++;
+            System.out.println("Passageiro embarcado");
+        }
+    }
+
+    public void registrarSaida() {
+        if (totalPassageiros == 0) {
+            lotado = false;
+            System.out.println("Ônibus vazio! Não há passageiros para sair.");
+        } else {
+            totalPassageiros--;
+            System.out.println("Passageiro desembarcado");
+        }
+    }
+
+    public void mostrarSituacao() {
+        System.out.println("Passageiros atuais: " + totalPassageiros);
+        System.out.println("Lugares disponíveis: " + (LIMITE - totalPassageiros));
+        if (totalPassageiros >= 1 && totalPassageiros <= 44) {
+            System.out.println("Satus: Movimento normal");
+        } else if (totalPassageiros == 0) {
+            System.out.println("Satus: " + (lotado ? "Lotado" : "Vazio"));
+        } else if (totalPassageiros == 45) {
+            System.out.println("Satus: " + (lotado ? "Lotado" : "Vazio"));
+        }
+    }
+
+    public void resetar() {
+        totalPassageiros = 0;
+        lotado = false;
+        System.out.println("Passageiros zerados");
+    }
 }
--- SenacCursoJava/Exercicio02CatracaOnibus/src/senac/curso/java/exercicio/catracapoo/SistemaCatracaPoo.java
+++ SenacCursoJava/Exercicio02CatracaOnibus/src/senac/curso/java/exercicio/catracapoo/SistemaCatracaPoo.java
@@ -1,7 +1,53 @@
 package senac.curso.java.exercicio.catracapoo;
 
+import java.util.Scanner;
+
 public class SistemaCatracaPoo {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Catraca onibus = new Catraca();
+
+        String opcao;
+
+        do {
+            System.out.print("\n===== CATRACA ELETRÔNICA =====\n" +
+                    "1 - Registrar entrada de passageiro\n" +
+                    "2 - Registrar saída de passageiro\n" +
+                    "3 - Mostrar situação atual do ônibus\n" +
+                    "4 - Resetar ônibus (zerar passageiros)\n" +
+                    "5 - Sair\n\n" +
+                    "Escolha uma opção: ");
+            opcao = input.nextLine();
+            switch (opcao) {
+                case "1":
+                    System.out.println("- Registrar entrada de passageiro");
+                    onibus.registrarEntrada();
+                    break;
+
+                case "2":
+                    System.out.println(" - Registrar saída de passageiro");
+                    onibus.registrarSaida();
+                    break;
+
+                case "3":
+                    System.out.println(" - Mostrar situação atual do ônibus");
+                    onibus.mostrarSituacao();
+                    break;
+
+                case "4":
+                    System.out.println(" - Resetar ônibus");
+                    onibus.resetar();
+                    break;
+
+                case "5":
+                    System.out.println(" - Sair");
+                    System.out.println("Saindo... Até logo!");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (!opcao.equals("5"));
 
     }
 }
