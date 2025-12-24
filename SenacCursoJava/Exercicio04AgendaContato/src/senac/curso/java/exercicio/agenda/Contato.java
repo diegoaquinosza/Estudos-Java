a9a3776939adf7be1fbb1897a8d50b450535bc79
diegoaquinosza <diegoaquinosza@gmail.com>
2025-12-24 17:17:44 -0300

    wip(Agenda): implementa estrutura inicial de adicao de contatos

--- SenacCursoJava/Exercicio04AgendaContato/src/senac/curso/java/exercicio/agenda/Contato.java
+++ SenacCursoJava/Exercicio04AgendaContato/src/senac/curso/java/exercicio/agenda/Contato.java
@@ -1,4 +1,48 @@
 package senac.curso.java.exercicio.agenda;
 
+import java.util.Scanner;
+
 public class Contato {
+    Scanner input = new Scanner(System.in);
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Contato(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        System.out.print("Digite o nome do contato: ");
+        this.nome = nome = input.nextLine();
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        System.out.print("Digite o número do telefone: ");
+        this.telefone = telefone = input.nextLine();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        System.out.print("Digite o endereço de e-mail: ");
+        this.email = email  = input.nextLine();
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome + " | Telefone: " + this.telefone + " | E-mail: " + this.email;
+    }
 }
--- SenacCursoJava/Exercicio04AgendaContato/src/senac/curso/java/exercicio/agenda/GerenciadorAgenda.java
+++ SenacCursoJava/Exercicio04AgendaContato/src/senac/curso/java/exercicio/agenda/GerenciadorAgenda.java
@@ -1,4 +1,46 @@
 package senac.curso.java.exercicio.agenda;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GerenciadorAgenda {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Contato> contatos = new ArrayList<>();
+
+        String opcao;
+        do {
+            System.out.print("\n" +
+                    "==== Menu Agenda ====\n" +
+                    "1 - Adicionar contato\n" +
+                    "2 - Listar contatos\n" +
+                    "3 - Remover contato\n" +
+                    "4 - Sair\n\n" +
+                    "Escolha uma opcao: ");
+            opcao = input.nextLine();
+            switch (opcao) {
+                case "1":
+                    System.out.println("Adicionando contato...");
+                    Contato novoContato = new Contato("","","");
+                    novoContato.setNome("nome");
+                    novoContato.setTelefone("telefone");
+                    novoContato.setEmail("email");
+                    contatos.add(novoContato);
+                    System.out.println("...Contato adicionado com sucesso!");
+                    break;
+                case "2":
+                    System.out.println("Listando contatos...");
+                    break;
+                case "3":
+                    System.out.println("Removendo contato...");
+                    break;
+                case "4":
+                    System.out.println("Saindo... Até logo!");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+
+        } while (!opcao.equals("4"));
+    }
 }
