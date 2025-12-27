53ee31e55a5837b14f483e2ca4f1a0deadecf82b
diegoaquinosza <diegoaquinosza@gmail.com>
2025-12-27 18:14:09 -0300

    chore(Aula05): estrutura inicial projeto colaborador

---
+++ IftoDisciplinaPoo/PooiftoAula05ProjetoColaborador/src/ifto/poo/aula05/projetocolaborador/Programa.java
@@ -0,0 +1,12 @@
+package ifto.poo.aula05.projetocolaborador;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Colaborador> lista = new ArrayList<>();
+
+    }
+}
