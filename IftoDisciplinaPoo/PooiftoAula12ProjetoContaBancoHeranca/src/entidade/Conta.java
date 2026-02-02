2880f883f0cf045db28e54692f23e3956ab49d08
diegoaquinosza <diegoaquinosza@gmail.com>
2026-02-02 19:01:53 -0300

    wip(Aula12): estrutura inicial da classe base Conta para sistema bancario

---
+++ IftoDisciplinaPoo/PooiftoAula12ProjetoContaBancoHeranca/src/entidade/Conta.java
@@ -0,0 +1,13 @@
+package entidade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conta {
+    private String numConta;
+    private String titular;
+    private double saldo;
+    private static List<Conta> lista = new ArrayList<>();
+}
+
+
