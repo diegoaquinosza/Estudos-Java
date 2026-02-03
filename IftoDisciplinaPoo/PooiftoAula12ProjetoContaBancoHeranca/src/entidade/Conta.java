4cc17f247aa3560b9c8fc2615bf5436c8e4ba0e0
diegoaquinosza <diegoaquinosza@gmail.com>
2026-02-03 12:08:54 -0300

    feat(Aula12): implementa classe Conta com correcoes no construtor e tipos de retorno

--- IftoDisciplinaPoo/PooiftoAula12ProjetoContaBancoHeranca/src/entidade/Conta.java
+++ IftoDisciplinaPoo/PooiftoAula12ProjetoContaBancoHeranca/src/entidade/Conta.java
@@ -8,6 +8,64 @@ public class Conta {
     private String titular;
     private double saldo;
     private static List<Conta> lista = new ArrayList<>();
+
+    //Construtores
+    public Conta() {
+    }
+
+    public Conta(String numConta, String titular, double saldo) {
+        this.numConta = numConta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    //Getters e Setters
+    public String getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(String numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public static List<Conta> getLista() {
+        return lista;
+    }
+
+    public static void setLista(List<Conta> lista) {
+        Conta.lista = lista;
+    }
+
+    //Métodos
+    public void criarCadastro(){}
+
+    public void saque(double valor){}
+
+    public void deposito(double valor){}
+
+    public void listar(int tipo){}
+
+    public Conta pesquisar(String numConta, int tipo){
+        return null;
+    }
+
 }
+
 
 
